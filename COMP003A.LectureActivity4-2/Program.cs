b3d93fb5a6a109f3786d44956199557207b836c3
SalinasJason <jasonsalinas770@gmail.com>
2025-02-03 17:10:26 -0800

    Added CalculateSum Method and DisplayArray Method

--- COMP003A.LectureActivity4-2/Program.cs
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -8,7 +8,7 @@ namespace COMP003A.LectureActivity4_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
 
         /// <summary> 
@@ -17,9 +17,34 @@ namespace COMP003A.LectureActivity4_2
 
         static void GreetUser()
         {
-            Console.WriteLine("Enter your name: ");
+            Console.Write("Enter your name: ");
             string name = Console.ReadLine(); // Store user input in 'name'
             Console.WriteLine($"Hello, {name}! Welcome to the Methoda Activiy.");
         }
+
+        /// <summary>
+        /// Calculates the sum of two numbers and returns the result
+        /// </summary>
+        /// <paran name="num1">Integer Input Number 1</paran>
+        /// <paran name="num2">Integer Input Number 2</paran>
+        /// <returns>Integer sum of the two integer inputs</returns>
+        static int CalculateSum(int num1, int num2)
+        {
+            return num1 + num2; // Add the two numbers and return the result
+        }
+
+        /// <summary>
+        /// Displays the contents of an array.
+        /// </summary>
+        /// <param name="numbers">Array collection of integers</param>
+        static void DisplayArray(int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+
     }
 }
