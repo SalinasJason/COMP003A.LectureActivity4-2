68aa38d1427c577e4e5d77c70e283d06b88d05d4
SalinasJason <jasonsalinas770@gmail.com>
2025-02-03 16:20:29 -0800

    Added author comments and GreetUser Method

--- COMP003A.LectureActivity4-2/Program.cs
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace COMP003A.LectureActivity4_2
+﻿// Author: Jason Salinas
+// Course: COMP003A
+// Faculty: Jonathan Cruz
+// Purpose: Practice implementing methods in C#
+namespace COMP003A.LectureActivity4_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        /// <summary> 
+        /// Greets the user by name.
+        /// </summary
+
+        static void GreetUser()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine(); // Store user input in 'name'
+            Console.WriteLine($"Hello, {name}! Welcome to the Methoda Activiy.");
         }
     }
 }
